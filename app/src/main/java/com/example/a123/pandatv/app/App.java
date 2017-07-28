a9e3e37971aeffac172978f36771d8fc368a4b68
feng1056 <1178036973@qq.com>
2017-07-28 09:39:10 +0800

    项目

--- app/src/main/java/com/example/a123/pandatv/app/App.java
+++ app/src/main/java/com/example/a123/pandatv/app/App.java
@@ -4,11 +4,10 @@ package com.example.a123.pandatv.app;
 import android.app.Application;
 
 import com.example.a123.pandatv.base.BaseActivity;
-import com.example.a123.pandatv.net.IHttp;
 
 public class App extends Application {
     public  static BaseActivity context=null;
-    public static IHttp iHttp;
+
     @Override
     public void onCreate() {
         super.onCreate();
