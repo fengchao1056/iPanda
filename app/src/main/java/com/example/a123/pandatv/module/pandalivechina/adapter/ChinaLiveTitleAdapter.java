fef25978f072b6f1a7abbbb180a668f8437c7913
JackSir1 <15128150932@qq.com>
2017-07-31 15:13:30 +0800

    直播中国和登录

--- app/src/main/java/com/example/a123/pandatv/module/pandalivechina/adapter/ChinaLiveTitleAdapter.java
+++ app/src/main/java/com/example/a123/pandatv/module/pandalivechina/adapter/ChinaLiveTitleAdapter.java
@@ -3,6 +3,7 @@ package com.example.a123.pandatv.module.pandalivechina.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
 import com.example.a123.pandatv.module.pandalivechina.chilefragment.LiveChinasFragment;
 
 import java.util.List;
@@ -14,10 +15,11 @@ import java.util.List;
 public class ChinaLiveTitleAdapter extends FragmentStatePagerAdapter {
     private List<LiveChinasFragment> liveChinasFragments;
     private List<String> titles;
+
     public ChinaLiveTitleAdapter(FragmentManager fm, List<LiveChinasFragment> liveChinasFragments, List<String> titles) {
         super(fm);
-        this.liveChinasFragments=liveChinasFragments;
-        this.titles=titles;
+        this.liveChinasFragments = liveChinasFragments;
+        this.titles = titles;
     }
 
     @Override
@@ -27,7 +29,7 @@ public class ChinaLiveTitleAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return liveChinasFragments.isEmpty()?0:liveChinasFragments.size();
+        return liveChinasFragments.isEmpty() ? 0 : liveChinasFragments.size();
     }
 
     @Override
