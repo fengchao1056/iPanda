a67f0be64481cc6733c65bd9f1a8ef50e45e7745
JackSir1 <15128150932@qq.com>
2017-07-31 14:58:41 +0800

    直播中国和登录

---
+++ app/src/main/java/com/example/a123/pandatv/module/personcenter/CollectActivity.java
@@ -0,0 +1,29 @@
+package com.example.a123.pandatv.module.personcenter;
+
+import com.example.a123.pandatv.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+
+public class CollectActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void setIntent() {
+
+    }
+}
---
+++ app/src/main/java/com/example/a123/pandatv/module/personcenter/PersonActivity.java
@@ -0,0 +1,30 @@
+package com.example.a123.pandatv.module.personcenter;
+
+import com.example.a123.pandatv.R;
+import com.example.a123.pandatv.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+
+public class PersonActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.personconte_itemr;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void setIntent() {
+
+    }
+}
